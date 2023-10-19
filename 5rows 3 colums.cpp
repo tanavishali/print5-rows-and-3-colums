f38967fefb95eef449b37decc9498b96a7535705
tanavishali <tanawishalrai5271@gmail.com>
2023-10-19 01:19:58 -0700

    first

---
+++ 5rows 3 colums.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	cpp program to display 5 rows and 3 colums
+int rows=5,colums=3;
+for(int i=1;i<=rows;i++){
+	for(int j=1;j<=colums;j++){
+		cout<<" * ";
+	}
+	cout<<endl;
+}
+	return 0;
+}
